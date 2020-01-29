b501dca3517b663a8425f64016f1359b006604f4
Marion Communal <mafarifironfon@hotmail.fr>
2020-01-29 19:55:26 +0100

    Add Style

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
 const postList = document.getElementById("listPost")
 
-function updateView(){
+function updateView() {
     fetch("http://localhost:3000/api/v1/todos/")
-    .then(res => res.json())
-    .then(data => listPost(data))
-    .catch(err => handleError(err));
+        .then(res => res.json())
+        .then(data => listPost(data))
+        .catch(err => handleError(err));
 }
-    function listPost (data){
-        let html = "";
-        for(let toDo of data){
-            html += `
+
+// function handleError(err) {
+//     console.error(err);
+// }
+
+function listPost(data) {
+    let html = "";
+    for (let toDo of data) {
+        html += `
             <div class="col text-center rounded border border-light">
-            Liste des posts
+            <h4>Liste des Posts</h4>
             <p>
                 <a class="btn btn-primary mt-3" data-toggle="collapse" href="#collapseExample" role="button"
                     aria-expanded="false" aria-controls="collapseExample">
@@ -24,12 +29,10 @@ function updateView(){
                 </div>
             </div>
         </div>
-            `        }
-            postList.innerHTML = html;
+            `
     }
+    postList.innerHTML = html;
+}
 
-    function handleError (err){
-        console.error(err);
-    }
 
-    updateView();
+updateView();
