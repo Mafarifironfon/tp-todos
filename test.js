b4c19dc0438e6bb2b08609beb52f9647d345afeb
Marion Communal <mafarifironfon@hotmail.fr>
2020-01-30 21:35:33 +0100

    Change Style

--- test.js
+++ test.js
@@ -1,10 +1,47 @@
 const postList = document.getElementById("listPost");
 const form = document.querySelector('form');
-const $ajouter = document.querySelector(".Ajouter");
+const $ajouter = document.querySelector(".plus");
 
-// Ajouter un Todo
+// RECUPERER LA LISTE DES TODOS
+function getPost() {
+    fetch("http://localhost:3000/api/v1/todos/", {
+            method: "GET"
+        })
+        .then(res => res.json())
+        .then(data => listPost(data))
+        .catch(err => handleError(err));
+}
+
+// MESSAGE D'ERREUR
+function handleError(err) {
+    console.error(err);
+}
+
+// AFFICHER LA LISTE DES POSTS 
+function listPost(data) {
+    let html = "";
+    for (let toDo of data) {
+        html += `
+
+            <div class="d-flex justify-content-center align-items-center">
+                <a class="btn btn-light mr-2 mt-3" data-toggle="collapse" href="#collapseExample${toDo.id}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                   <h5>${toDo.title}</h5> 
+                </a>
+                <a href="#"><i data-id=${toDo.id} class="far fa-trash-alt pt-4"></i></a>
+            </div>
+            <div class="collapse" id="collapseExample${toDo.id}">
+                <div class="card card-body">
+                <h5>${toDo.content}</h5> 
+                </div>
+            </div>`
+    }
+    postList.innerHTML = html;
+}
+
+getPost();
+
+// AJOUTER UN TODO
 $ajouter.addEventListener("click", addPost);
-// Afficher un nouveau todo
 function addPost() {
 const title = document.getElementById("exampleInputEmail1").value;
 const content = document.getElementById("exampleInputPassword1").value;
@@ -25,43 +62,6 @@ const data = {
         .catch(err => handleError(err));
 }
 
-// Récupérer la liste des Todos
-function getPost() {
-    fetch("http://localhost:3000/api/v1/todos/", {
-            method: "GET"
-        })
-        .then(res => res.json())
-        .then(data => listPost(data))
-        .catch(err => handleError(err));
-}
-
-function handleError(err) {
-    console.error(err);
-}
-
-// Afficher la liste des posts
-function listPost(data) {
-    let html = "";
-    for (let toDo of data) {
-        html += `
-
-            <p>
-                <a class="btn btn-primary mt-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-                    ${toDo.title}
-                </a>
-                <a href="#"><i data-id=${toDo.id} class="fas fa-trash text-light"></i></a>
-            </p>
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                ${toDo.content}
-                </div>
-            </div>`
-    }
-    postList.innerHTML = html;
-}
-
-getPost();
-
 document.addEventListener('click', function (e) {
     const $target = e.target;
     if ($target.hasAttribute("data-id")) {
@@ -81,27 +81,4 @@ function deleteTodo($id) {
         .then(res => res.json())
         .then(data => getTodoList(data))
         .catch(err => handleError(err));
-
-}
-
-document.addEventListener('click', function (e) {
-    const $target = e.target;
-    if ($target.hasAttribute("data-id")) {
-      const $id = Number($target.getAttribute("data-id"));
-        deleteTodo($id);
-    }
-});
-
-function deleteTodo($id) {
-
-    fetch(`http://localhost:3000/api/v1/todos/${$id}`, {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(res => res.json())
-        .then(data => getTodoList(data))
-        .catch(err => handleError(err));
-
 }
